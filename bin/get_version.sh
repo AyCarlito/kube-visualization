c8698d8e3f9aec399ac4c9b05ba44420f3d94ef1
AyCarlito <>
2025-03-23 22:05:23 +0000

    refactor: Git tag should be x.y.z

--- bin/get_version.sh
+++ bin/get_version.sh
@@ -1,54 +1,44 @@
 #!/bin/bash
 
-# Version defined as major.minor.patch-build
-# - major & minor numbers are taken from the last git tag
-# - patch comes from the git describe "distance" from the last tag
-# - build is either:
-#      - $BUILD_ID (when main or release branches are built in CI)
-#      - dev.$BRANCH_NAME.$BUILD_ID (when dev branches are built in CI)
-#      - dev.branch.sha (when building outside CI; BRANCH_NAME obtained from git)
-#
-# Note that branch names are "mangled" to keep the overall version SemVer compatible.
-#
-# *** If the git repo is not tagged, it will default to 0.0.0-X ***
-#
+# Return a SemVer compliant version for the current commit.
+# - major, minor and patch numbers are the latest git tag.
+# "-build" is appended in the case of:
+#       - local builds on all branches
+#       - CI builds on non-release branches
+# build is defined as:
+#       - dev.$BRANCH_NAME.$BUILD_ID (CI)
+#       - dev.branch.sha (Local)
 
-short_desc=$(git describe --abbrev=0 --tags 2>/dev/null) || short_desc="0.0"
-long_desc=$(git describe --long --tags 2>/dev/null) || long_desc="0.0-0"
-if [ $short_desc = "0.0" ]; then
-    >&2 echo -e "\n*** WARNING: No tag found, using $short_desc ***\n"
-fi
+tag=$(git describe --tags --abbrev=0)
 
-readonly major=$(echo ${short_desc} | cut --delimiter=. --fields=1)
-readonly minor=$(echo ${short_desc} | cut --delimiter=. --fields=2)
-readonly patch=$(echo ${long_desc}  | cut --delimiter=- --fields=2)
-
-# Handle BRANCH_NAME being defined (building in CI), or undefined (local builds)
+current_branch="${BRANCH_NAME}"
 if [ -z "${BRANCH_NAME}" ]; then
-    current_branch="$(git symbolic-ref --short HEAD 2>/dev/null)" ||
-        current_branch="detached" # detached HEAD
-else
-    current_branch=${BRANCH_NAME}
+    current_branch="$(git symbolic-ref --short HEAD)" || current_branch="detached" # detached HEAD
 fi
 
-# Generate the "branch" part of the version
-readonly branch_number_prefix="$(echo "${current_branch}" | sed -e 's#\(^[0-9]*\).*#\1#')"
+# If the branch is prefixed with a number corresponding to an issue, use it.
+# Otherwise we use what is remaining after stripping the non alphanumeric characters.
+branch_number_prefix="${current_branch%%[!0-9]*}"
+readonly branch_number_prefix
 if [ "${branch_number_prefix}" ]; then
     branch_prefix="${branch_number_prefix}"
 else
-    branch_prefix="$(echo "${current_branch}" | sed -e 's/^dev[^[:alnum:]]//g' -e 's/[^[:alnum:]]//g')"
+    branch_prefix="${current_branch//[^[:alnum:]]/}"
 fi
 
-# Use BUILD_ID if it's defined (CI), otherwise use SHA1 (local builds).
+# SHA for local builds.
 if [ -z "${BUILD_ID}" ]; then
     current_sha="$(git rev-parse --short HEAD)"
     build="dev.${branch_prefix}.${current_sha}"
 else
-    if [ "${current_branch}" = "main" ] || [ "${current_branch:0:7}" = "release" ]; then
-        build=${BUILD_ID}
-    else
-        build="dev.${branch_prefix}.${BUILD_ID}"
-    fi
+    # Build ID for CI builds.
+    build="dev.${branch_prefix}.${BUILD_ID}"
 fi
 
-echo "${major}.${minor}.${patch}-${build}"
+# Special case when building on a release branch
+version="${tag}-${build}"
+if [ "${current_branch:0:7}" = "release" ]; then
+    version="${tag}"
+fi
+
+echo "${version}"
